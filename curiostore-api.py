29757626c05c33e7f93191f93f23a38a09ee798b
Andrew Smith <smith.andrew.l@gmail.com>
2018-08-17 20:31:29 -0500

    Added barebones authentication using bottlejwt.

--- curiostore-api.py
+++ curiostore-api.py
@@ -1,7 +1,18 @@
-from bottle import route, run
+from bottle import get, install, run
+from bottlejwt import JwtPlugin
 
-@route('/hello')
-def hello():
-    return "Hello World!"
+def validation(auth, auth_value):
+    print(auth, auth_value)
+    return True
 
-run(host='localhost', port=8080, debug=True)
+@get("/create")
+def create():
+    return JwtPlugin.encode({'name': 'pepito'})
+
+@get("/", auth="any values and types")
+def example(auth):  # auth argument is optional!
+    return "ok"
+   
+
+install(JwtPlugin(validation, 'secret', algorithm='HS256'))
+run(host="0.0.0.0", port="9988")
