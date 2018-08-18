c9de0361566ac0b44572cad15d251187d83a33c7
Andrew Smith <smith.andrew.l@gmail.com>
2018-08-18 11:31:43 -0500

    Added more test user accounts.

--- curiostore-api.py
+++ curiostore-api.py
@@ -28,9 +28,14 @@ def validation(auth, auth_value):
 
 @app.get("/create")
 def create(db):
-    user = db.query(User).first()
-    return {'id': user.id, 'email': user.email, 'display_name': user.display_name, 'password_hash': user.password_hash}
-    return JwtPlugin.encode({'name': 'pepito'})
+    users = db.query(User).all()
+
+    result = {}
+
+    for user in users:
+        result[user.id] = {'id': user.id, 'email': user.email, 'display_name': user.display_name, 'password_hash': user.password_hash}
+
+    return result
 
 @app.get("/", auth="any values and types")
 def example(auth):  # auth argument is optional!
@@ -53,7 +58,10 @@ Session = sessionmaker(bind=engine)
 session=Session()
 Base.metadata.create_all(engine)
 
-session.add(User(email='tbobs@place.com', display_name='Tom Bob', password_hash='No'))
+session.add(User(email='tbobs@place.com', display_name='Tim Bob', password_hash='No'))
+session.add(User(email='bbobs@place.com', display_name='Paul Bob', password_hash='No'))
+session.add(User(email='jbobs@place.com', display_name='John Bob', password_hash='No'))
+session.add(User(email='rbobs@place.com', display_name='Rue Bob', password_hash='No'))
 session.commit()
 
 app.install(JwtPlugin(validation, 'secret', algorithm='HS256'))
