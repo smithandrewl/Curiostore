7646d57dbf5ad2cafb3f4bde0489f9df9b4de3ff
Andrew Smith <smith.andrew.l@gmail.com>
2018-08-20 19:05:36 -0500

    Added a barebones login component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes, Router} from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     RouterModule.forRoot(
