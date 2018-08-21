fd674417ed98962b2b8b0322f3dccc7490d76a37
Andrew Smith <smith.andrew.l@gmail.com>
2018-08-20 19:01:57 -0500

    Added routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes, Router} from '@angular/router';
+
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule
   ],
   providers: [],
