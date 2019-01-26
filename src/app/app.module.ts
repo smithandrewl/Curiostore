78543a770e6c0513a14352f0aa6d53df41734153
Andrew Smith <smith.andrew.l@gmail.com>
2019-01-26 12:26:08 -0600

    Reformatted app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { RouterModule, Routes, Router} from '@angular/router';
-import { LoginComponent } from './login/login.component';
-
-import { JwtModule } from '@auth0/angular-jwt';
+import { BrowserModule    } from '@angular/platform-browser';
+import { NgModule         } from '@angular/core';
+import { FormsModule      } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { FormsModule} from '@angular/forms';
-import { PortalComponent } from './portal/portal.component';
-import { AddComponent } from './portal/add/add.component';
-import { CollectionsComponent } from './portal/collections/collections.component';
-import { LookupComponent } from './portal/lookup/lookup.component';
-import { CollectionComponent } from './portal/collections/collection/collection.component';
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-import { ItemComponent } from './portal/collections/collection/item/item.component';
+import { AppComponent         } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent       } from './login/login.component';
 
+import { PortalComponent      } from './portal/portal.component';
+import { AddComponent         } from './portal/add/add.component';
+import { CollectionsComponent } from './portal/collections/collections.component';
+import { LookupComponent      } from './portal/lookup/lookup.component';
+import { CollectionComponent  } from './portal/collections/collection/collection.component';
+import { ItemComponent        } from './portal/collections/collection/item/item.component';
+
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 import { StoreModule } from '@ngrx/store';
 
@@ -28,11 +26,27 @@ const appRoutes: Routes = [
     path: 'portal',
     component: PortalComponent,
     children: [
-      { path: '', redirectTo: 'collections', pathMatch: 'full'},
-      { path: 'add', component: AddComponent},
-      { path: 'collections', component: CollectionsComponent},
-      { path: 'collections/:id', component: CollectionComponent},
-      { path: 'lookup', component: LookupComponent}
+      {
+        path:       '',
+        redirectTo: 'collections',
+        pathMatch:  'full'
+      },
+      {
+        path:      'add',
+        component: AddComponent
+      },
+      {
+        path:      'collections',
+        component: CollectionsComponent
+      },
+      {
+        path:      'collections/:id',
+        component: CollectionComponent
+      },
+      {
+        path:      'lookup',
+        component: LookupComponent
+      }
     ]
   }
 ];
@@ -49,17 +63,21 @@ const appRoutes: Routes = [
     ItemComponent
   ],
   imports: [
-    LoggerModule.forRoot({level: NgxLoggerLevel.DEBUG}),
+    LoggerModule.forRoot(
+      {
+        level: NgxLoggerLevel.DEBUG
+      }
+    ),
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing: false}
+      { enableTracing: false }
     ),
     BrowserModule,
     StoreModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
