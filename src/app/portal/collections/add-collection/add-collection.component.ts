0e3ce11be920309bc80e166ed0a678e8dc3c2201
Andrew Smith <smith.andrew.l@gmail.com>
2019-02-09 15:50:17 -0600

    Added a working cancel button to the Add a new collection page.

--- src/app/portal/collections/add-collection/add-collection.component.ts
+++ src/app/portal/collections/add-collection/add-collection.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, HashLocationStrategy, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'app-add-collection',
   templateUrl: './add-collection.component.html',
-  styleUrls: ['./add-collection.component.css']
+  styleUrls: ['./add-collection.component.css'],
+  providers: [Location, {provide: LocationStrategy,  useClass: PathLocationStrategy}]
 })
 export class AddCollectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
-  ngOnInit() {
+  ngOnInit() {  }
+
+  public cancel() {
+    this._location.back();
   }
 
 }
