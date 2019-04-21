cd0b04283f9f6b5cb9f3eb0b7d6d4f8c791735a4
Andrew Smith <smith.andrew.l@gmail.com>
2019-04-21 17:02:20 -0500

    Added an observable of all collections.

--- src/app/shared/facades/collections-facade.service.ts
+++ src/app/shared/facades/collections-facade.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {adapter} from '../../store/entities/collection/collection.reducer';
+import {ApplicationState} from '../../store/reducers';
+import {Store} from '@ngrx/store';
+
+import * as fromCollectionsReducer from '../../store/entities/collection/collection.reducer';
+import {Collection} from '../../store/entities/collection/collection.model';
+import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionsFacadeService {
 
-  constructor() { }
+  public readonly selectAllCollection$: Observable<Collection[]>;
+
+
+
+  constructor(private store: Store<ApplicationState>) {
+    this.selectAllCollection$ = store.select(fromCollectionsReducer.selectAll);
+  }
 }
