ba3057550c84e8e80e30280bab1584aa11861f8f
Andrew Smith <smith.andrew.l@gmail.com>
2019-04-29 17:38:14 -0500

    Code cleanup.

--- src/app/portal/collections/add-collection/add-collection.component.ts
+++ src/app/portal/collections/add-collection/add-collection.component.ts
@@ -1,39 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import {Location, HashLocationStrategy, LocationStrategy, PathLocationStrategy} from '@angular/common';
-import {CollectionsService} from '../../../shared/services/collections.service';
-import {ApplicationState} from '../../../store/reducers';
-import {Store} from '@ngrx/store';
-import {AddCollection} from '../../../store/entities/collection/collection.actions';
-import {Collection} from '../../../shared/models/models';
-import {Form, FormControl, FormGroup} from '@angular/forms';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common';
+
+import { CollectionsService } from '../../../shared/services/collections.service';
+import { ApplicationState   } from '../../../store/reducers';
+import { Store              } from '@ngrx/store';
+import { AddCollection      } from '../../../store/entities/collection/collection.actions';
+import { Collection         } from '../../../shared/models/models';
+
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-add-collection',
+  selector:    'app-add-collection',
   templateUrl: './add-collection.component.html',
-  styleUrls: ['./add-collection.component.css'],
-  providers: [Location, {provide: LocationStrategy,  useClass: PathLocationStrategy}]
+  styleUrls:   ['./add-collection.component.css'],
+  providers:   [Location, {provide: LocationStrategy,  useClass: PathLocationStrategy}]
 })
 export class AddCollectionComponent implements OnInit {
 
-  addCollectionForm: FormGroup;
-  nameControl: FormControl;
+  addCollectionForm:  FormGroup;
+  nameControl:        FormControl;
   descriptionControl: FormControl;
 
 
 
-  constructor(private _location: Location, private collections: CollectionsService, private store: Store<ApplicationState>) { }
+  constructor(
+    private _location:   Location,
+    private collections: CollectionsService,
+    private store:       Store<ApplicationState>
+  ) { }
 
   ngOnInit() {
-
-
-    this.nameControl = new FormControl('');
+    this.nameControl        = new FormControl('');
     this.descriptionControl = new FormControl('');
 
     this.addCollectionForm = new FormGroup({
-      name: this.nameControl,
+      name:        this.nameControl,
       description: this.descriptionControl
     });
-
   }
 
   public cancel() {
@@ -42,13 +56,22 @@ export class AddCollectionComponent implements OnInit {
 
   save() {
     if(this.addCollectionForm.valid) {
-      const name = this.addCollectionForm.get('name').value;
+      const name        = this.addCollectionForm.get('name').value;
       const description = this.addCollectionForm.get('description').value;
 
 
       this.collections.createCollection('Rue Bob', name, description).subscribe(
         () => {
-          this.store.dispatch(new AddCollection({collection: new Collection(Math.random(), name, description)}));
+          this.store.dispatch(
+            new AddCollection(
+              {
+                collection: new Collection(
+                  Math.random(),
+                  name, description
+                )
+              }
+            )
+          );
         }
       );
     } else {
