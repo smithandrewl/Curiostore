11bfdbb91b38bfe9f3ad6ca9c8b2e345c5153425
Andrew Smith <smith.andrew.l@gmail.com>
2019-05-04 10:47:23 -0500

    Added the shell of a page alert component to display top of the page messages.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { PageAlertComponent } from './page-alert/page-alert.component';
 
 @NgModule({
-  declarations: [SpinnerComponent],
+  declarations: [SpinnerComponent, PageAlertComponent],
   imports: [
     CommonModule
   ],
---
+++ src/app/components/page-alert/page-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-alert',
+  templateUrl: './page-alert.component.html',
+  styleUrls: ['./page-alert.component.css']
+})
+export class PageAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
