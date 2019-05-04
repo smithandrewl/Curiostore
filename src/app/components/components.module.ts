f84c92e9680c7d7b9c5f2539efd799d677b75846
Andrew Smith <smith.andrew.l@gmail.com>
2019-05-04 09:36:24 -0500

    Added a the shell of a spinner component.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SpinnerComponent],
   imports: [
     CommonModule
   ]
