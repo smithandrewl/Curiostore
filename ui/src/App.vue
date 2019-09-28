caa9b25a3387b35103e12115092c883724c1aa55
Andrew Smith <smith.andrew.l@gmail.com>
2019-09-28 09:35:44 -0500

    Removed Vuetify boilerplate and branding.

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,17 +2,10 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Heading:&nbsp</span>
+        <span class="font-weight-light">Sub-heading</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
@@ -30,8 +23,6 @@ export default Vue.extend({
   components: {
     HelloWorld,
   },
-  data: () => ({
-    //
-  }),
+  data: () => ({}),
 });
 </script>
