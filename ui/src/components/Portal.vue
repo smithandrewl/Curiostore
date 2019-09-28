22ec0839af39b1907f6f6cd24b17497c087fe6bd
Andrew Smith <smith.andrew.l@gmail.com>
2019-09-28 14:38:39 -0500

    Added basic routing with one path and component.

---
+++ ui/src/components/Portal.vue
@@ -0,0 +1,254 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row
+            v-if="item.heading"
+            :key="item.heading"
+            align="center"
+          >
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col
+              cols="6"
+              class="text-center"
+            >
+              <a
+                href="#!"
+                class="body-2 black--text"
+              >EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            <v-list-item
+              v-for="(child, i) in item.children"
+              :key="i"
+              @click=""
+            >
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item
+            v-else
+            :key="item.text"
+            @click=""
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Curiostore</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-search"
+        label="Item#"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <div class="flex-grow-1"></div>
+      Ful L. Name
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+    <v-content style="height: 100%;background-color: rgb(3, 46, 13);box-shadow: inset 0 0 5rem rgb(1, 19, 0);">
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-btn
+      bottom
+      color="primary"
+      dark
+      fab
+      fixed
+      right
+      @click="dialog = !dialog"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-dialog
+      v-model="dialog"
+      width="800px"
+    >
+      <v-card>
+        <v-card-title class="primary text--secondary">
+          Create contact
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              class="align-center justify-space-between"
+              cols="12"
+            >
+              <v-row align="center">
+                <v-avatar
+                  size="40px"
+                  class="mr-4"
+                >
+                  <img
+                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
+                    alt=""
+                  >
+                </v-avatar>
+                <v-text-field
+                  placeholder="Name"
+                ></v-text-field>
+              </v-row>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                prepend-icon="mdi-business"
+                placeholder="Company"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                placeholder="Job title"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                prepend-icon="mdi-mail"
+                placeholder="Email"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                type="tel"
+                prepend-icon="mdi-phone"
+                placeholder="(000) 000 - 0000"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                prepend-icon="mdi-notes"
+                placeholder="Notes"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-card-actions>
+          <v-btn
+            text
+            color="primary"
+          >More</v-btn>
+          <div class="flex-grow-1"></div>
+          <v-btn
+            text
+            color="primary"
+            @click="dialog = false"
+          >Cancel</v-btn>
+          <v-btn
+            text
+            @click="dialog = false"
+          >Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+        data: () => ({
+            dialog: false,
+            drawer: null,
+            items: [
+                { icon: 'mdi-contacts', text: 'Contacts' },
+                { icon: 'mdi-history', text: 'Frequently contacted' },
+                { icon: 'mdi-content-copy', text: 'Duplicates' },
+                {
+                    icon: 'mdi-chevron-up',
+                    'icon-alt': 'mdi-chevron-down',
+                    text: 'Labels',
+                    model: true,
+                    children: [
+                        { icon: 'mdi-plus', text: 'Create label' },
+                    ],
+                },
+                {
+                    icon: 'mdi-chevron-up',
+                    'icon-alt': 'mdi-chevron-down',
+                    text: 'More',
+                    model: false,
+                    children: [
+                        { text: 'Import' },
+                        { text: 'Export' },
+                        { text: 'Print' },
+                        { text: 'Undo changes' },
+                        { text: 'Other contacts' },
+                    ],
+                },
+                { icon: 'mdi-settings', text: 'Settings' },
+                { icon: 'mdi-chat-bubble', text: 'Send feedback' },
+                { icon: 'mdi-help', text: 'Help' },
+                { icon: 'mdi-phonelink', text: 'App downloads' },
+                { icon: 'mdi-keyboard', text: 'Go to the old version' },
+            ],
+        }),
+    }
+</script>
--- ui/src/router.ts
+++ ui/src/router.ts
@@ -1,13 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-
+import Portal from "@/components/Portal.vue";
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-
+    {path: '/', component: Portal}
   ],
 });
