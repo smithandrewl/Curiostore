0b8e82aa682102eb2cc837c9d68823fb124ea930
Andrew Smith <smith.andrew.l@gmail.com>
2019-09-28 14:47:41 -0500

    Changed the routes of the portal and login components.

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {path: '/', component: Portal},
-    {path: '/login', component: Login}
+    {path: '/portal', component: Portal},
+    {path: '/', component: Login}
   ],
 });
