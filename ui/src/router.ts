693efb1c2eea696e853610fbc3eb9b98fce5a906
Andrew Smith <smith.andrew.l@gmail.com>
2019-09-28 14:40:59 -0500

    Added an empty component for the login screen.

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Portal from "@/components/Portal.vue";
+import Login from "@/components/Login.vue";
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {path: '/', component: Portal}
+    {path: '/', component: Portal},
+    {path: '/login', component: Login}
   ],
 });
