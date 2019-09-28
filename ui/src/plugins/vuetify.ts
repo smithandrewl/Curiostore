4bfe7e311ee27d27d944b6feef8d06c15564eda0
Andrew Smith <smith.andrew.l@gmail.com>
2019-09-28 09:54:24 -0500

    Added a theme for Vuetify

--- ui/src/plugins/vuetify.ts
+++ ui/src/plugins/vuetify.ts
@@ -4,7 +4,21 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#28A745',
+        secondary: '#FFFFFF',
+        accent: '#032E0D',
+        error: '#DC3545',
+        info: '#007BFF',
+        success: '#28A745',
+        warning: '#FFC107'
+      }
+    }
+  },
   icons: {
     iconfont: 'mdi',
   },
+
 });
