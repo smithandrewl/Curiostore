63b19b5307936163c88086df7524dfe29882f03a
Andrew Smith <smith.andrew.l@gmail.com>
2019-09-30 19:19:41 -0500

    Added default data to the store.

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -3,9 +3,27 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+interface ProfileSettings {
+  name: string;
+}
+
+interface Collection {
+  name: string
+  coverPhoto: string
+}
+
+
 export default new Vuex.Store({
   state: {
-
+    profile: <ProfileSettings>{
+      name: 'Ful L. Name',
+    },
+    collections: [
+      <Collection>{
+        name: 'Fossils',
+        coverPhoto: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+      },
+    ],
   },
   mutations: {
 
