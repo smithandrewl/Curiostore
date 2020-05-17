3363dd7f2fa7702ab452cc9e81333fcefd63bba6
Andrew Smith <smith.andrew.l@gmail.com>
2020-05-17 12:14:44 -0500

    Added a vuex module for auth.

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {auth} from '@/store/modules/auth';
 
 Vue.use(Vuex);
 
@@ -39,4 +40,7 @@ export default new Vuex.Store({
   actions: {
 
   },
+  modules: {
+    auth
+  }
 });
---
+++ ui/src/store/modules/auth.ts
@@ -0,0 +1,22 @@
+interface AuthState {
+
+}
+
+const InitialState = <AuthState> {
+
+};
+
+export const auth = {
+  namespaced: true,
+  state: () => InitialState,
+  getters: {
+
+  },
+  actions: {
+
+  },
+  mutations: {
+
+  },
+
+}
